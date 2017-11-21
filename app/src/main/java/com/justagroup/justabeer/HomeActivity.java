884f71ee8aa912d7269d706278967bdae900c30a
Timo <timo@overmark.org>
2017-11-21 15:28:30 +0100

    let's see if this fixes tthis

--- app/src/main/java/com/justagroup/justabeer/HomeActivity.java
+++ app/src/main/java/com/justagroup/justabeer/HomeActivity.java
@@ -6,10 +6,12 @@ import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
+import android.support.v7.widget.CardView;
 
 public class HomeActivity extends AppCompatActivity {
 
     private TextView mTextMessage;
+    private CardView mCardView;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -18,19 +20,20 @@ public class HomeActivity extends AppCompatActivity {
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
+                    //mTextMessage.setText(R.string.title_home);
+                   //mCardView.
                     return true;
                 case R.id.navigation_myhangouts:
-                    mTextMessage.setText(R.string.title_myhangouts);
+                    //mTextMessage.setText(R.string.title_myhangouts);
                     return true;
                 case R.id.navigation_addhangout:
-                    mTextMessage.setText(R.string.title_addhangout);
+                    //mTextMessage.setText(R.string.title_addhangout);
                     return true;
                 case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
+                    //mTextMessage.setText(R.string.title_notifications);
                     return true;
                 case R.id.navigation_profile:
-                    mTextMessage.setText(R.string.title_profile);
+                    //mTextMessage.setText(R.string.title_profile);
                     return true;
             }
             return false;
@@ -43,6 +46,7 @@ public class HomeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_home);
 
         mTextMessage = (TextView) findViewById(R.id.message);
+        mCardView = (CardView) findViewById(R.id.hangoutCard);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }

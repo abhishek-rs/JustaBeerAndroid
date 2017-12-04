0c8f141ef8931d78a77a04cfee8637e26ee3f20a
Abhishek Shetty <abhishek10.rs@gmail.com>
2017-12-04 01:39:01 +0100

    ugly and dumb hangouts detail done, merged changes from milda and zuzze

--- app/src/main/java/com/justagroup/justabeer/HangoutActivity.java
+++ app/src/main/java/com/justagroup/justabeer/HangoutActivity.java
@@ -6,12 +6,14 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.design.widget.TabLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 public class HangoutActivity extends AppCompatActivity {
@@ -33,6 +35,44 @@ public class HangoutActivity extends AppCompatActivity {
         people.setText("Andrea");
         TextView type = findViewById(R.id.hangout_type);
         type.setText("Beer");
+
+        TabLayout commentTabLayout = (TabLayout) findViewById(R.id.commentTabs);
+        TabLayout.Tab commentsTab = commentTabLayout.getTabAt(0);
+        TabLayout.Tab messagesTab = commentTabLayout.getTabAt(1);
+
+
+        final LinearLayout commentsLayout = (LinearLayout) findViewById(R.id.comments);
+        final LinearLayout messagesLayout = (LinearLayout) findViewById(R.id.messages);
+
+        commentTabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                Log.d("testing", "selected tab:" + tab.getText());
+                if (tab.getText().equals("Comments")) {
+                    commentsLayout.setVisibility(View.VISIBLE);
+                    messagesLayout.setVisibility(View.GONE);
+                } else {
+                    commentsLayout.setVisibility(View.GONE);
+                    messagesLayout.setVisibility(View.VISIBLE);
+                }
+
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+                Log.d("testing", "unselected tab:" + tab.getText());
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+                Log.d("testing", "reselected tab:" + tab.getText());
+            }
+        });
+
+
+
+
+
 
         backdrop.setImageResource(R.drawable.bar);
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);

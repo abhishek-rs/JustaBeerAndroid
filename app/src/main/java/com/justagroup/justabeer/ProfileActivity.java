02b9842e9ef0a0b3da8432e055b4150d2cc50d46
milda norkute <menulio.kiskis@gmail.com>
2017-12-08 22:12:08 +0100

    clicking on user profile and seeing info WORRRKS

--- app/src/main/java/com/justagroup/justabeer/ProfileActivity.java
+++ app/src/main/java/com/justagroup/justabeer/ProfileActivity.java
@@ -3,9 +3,20 @@ package com.justagroup.justabeer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
+import android.view.View;
+
+import com.squareup.picasso.Picasso;
 
 public class ProfileActivity extends AppCompatActivity {
+
+    private TextView userName;
+    private TextView userAge;
+    private TextView userGender;
+    private TextView userAbout;
+    //private TextView userImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,6 +25,27 @@ public class ProfileActivity extends AppCompatActivity {
 
         User user = (User) getIntent().getParcelableExtra("user");
         Log.d("user", "user = " + user);
-        Toast.makeText(this, "User = " + user.getFullName(), Toast.LENGTH_SHORT).show();
+        //Toast.makeText(this, "User = " + user.getFullName(), Toast.LENGTH_SHORT).show();
+        userName = (TextView) findViewById(R.id.user_card_title);
+        userAge = (TextView) findViewById(R.id.user_age);
+        userGender = (TextView) findViewById(R.id.user_gender);
+        userAbout = (TextView) findViewById(R.id.user_about);
+        //userImage = (ImageView).findViewById(R.id.user_image_view);
+
+
+        userName.setText(user.getFullName());
+        userAge.setText(Integer.toString(user.getAge()));
+        userGender.setText("Not defined in db");
+        userAbout.setText(user.getAbout());
+/*
+        if(!user.getPhoto().equals("")) {
+            Picasso
+                    //.with(ProfileActivity.this)
+                    .with(this)
+                    .load(user.getPhoto())
+                    .into(image);
+
+    }
+*/
     }
 }

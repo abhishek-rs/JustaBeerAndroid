8f8a5b1ccec8c03a36fa8d6c2e2b5077cba92783
Timo <timo@overmark.org>
2017-12-10 17:03:46 +0200

    Create hangout

--- app/src/main/java/com/justagroup/justabeer/Fragments/CreateHangoutFragment.java
+++ app/src/main/java/com/justagroup/justabeer/Fragments/CreateHangoutFragment.java
@@ -1,33 +1,52 @@
 package com.justagroup.justabeer.Fragments;
 
-import android.app.DatePickerDialog;
 import android.app.Dialog;
 import android.app.TimePickerDialog;
+import android.content.Intent;
 import android.content.Context;
 import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
-import android.text.Editable;
 import android.text.InputFilter;
-import android.text.TextWatcher;
 import android.text.format.DateFormat;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TimePicker;
-import java.util.Calendar;
 import android.widget.Toast;
+import android.widget.Button;
 
-import com.justagroup.justabeer.HomeActivity;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+import java.lang.reflect.Field;
+
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import com.justagroup.justabeer.Hangout;
+
 import com.justagroup.justabeer.R;
 
 /**
@@ -47,14 +66,6 @@ public class CreateHangoutFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
-
-    public String selectedType;
-    public String selectedPlace;
-    public String selectedDate;
-    public String selectedStartTime;
-    public String selectedEndTime;
-    public String selectedDescription;
-
 
     private OnFragmentInteractionListener mListener;
 
@@ -95,146 +106,165 @@ public class CreateHangoutFragment extends Fragment {
                              Bundle savedInstanceState) {
         final View view = inflater.inflate(R.layout.fragment_create_hangout, container, false);
 
-        //------- EVENT TYPE ----------
-        final RadioGroup eventTypeRadioGroup = (RadioGroup) view.findViewById(R.id.eventTypeRadio);
+        /*------- EVENT TYPE ----------
+        ImageView beer = (ImageView) view.findViewById(R.id.beerIcon);
+        ImageView coffee = (ImageView) view.findViewById(R.id.coffeeIcon);
+        ImageView food = (ImageView) view.findViewById(R.id.foodIcon);
+
+        RadioGroup eventTypeRadioGroup = (RadioGroup) view.findViewById(R.id.eventTypeRadio);
+        RadioButton radio1 = view.findViewById(R.id.beerRadio);
+        eventTypeRadioGroup.addView(radio1);
+        RadioButton radio2 = view.findViewById(R.id.coffeeRadio);
+        eventTypeRadioGroup.addView(radio2);
+        RadioButton radio3 = view.findViewById(R.id.foodRadio);
+        eventTypeRadioGroup.addView(radio3);
+
+        /*
+        //init event type radio group
+        RadioGroup eventTypeRadioGroup = (RadioGroup) view.findViewById(R.id.eventTypeRadio);
+        final String[] eventNames = new String[] {"Beer","Food","Sports"};
+        RadioButton radio1 = view.findViewById(R.id.beerRadio);
+        eventTypeRadioGroup.addView(radio1);
+        RadioButton radio2 = view.findViewById(R.id.foodRadio);
+        eventTypeRadioGroup.addView(radio2);
+        RadioButton radio3 = view.findViewById(R.id.sportsRadio);
+        eventTypeRadioGroup.addView(radio3);
 
         eventTypeRadioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(RadioGroup radioGroup, int ind) {
                 Log.d("event type index", ind + "");
+                RadioButton button = null;
 
-                if(eventTypeRadioGroup.getCheckedRadioButtonId()!=-1){
-                    int id= eventTypeRadioGroup.getCheckedRadioButtonId();
-                    View radioButton = eventTypeRadioGroup.findViewById(id);
-                    int radioId = radioGroup.indexOfChild(radioButton);
-                    RadioButton btn = (RadioButton) eventTypeRadioGroup.getChildAt(radioId);
-                    selectedType = (String) btn.getText();
+                for (int i = 0; i < eventNames.length; ++i) {
+                    if (ind == i) {
+                        //button selected
+                        button = (RadioButton) view.findViewById(ind);
+                        //button = (RadioButton) radioGroup.getChildAt(ind);
+                        button.setChecked(true);
+                    } else {
+                        //buttons that are not selected
+                        button = (RadioButton) view.findViewById(ind);
+                        button.setChecked(false);
+                    }
                 }
             }
-        });
-        //---------- DATE ------------
-        final RadioGroup eventDate = (RadioGroup) view.findViewById(R.id.dateRadio);
-
-        //init event type radio group
-        eventDate.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(RadioGroup radioGroup, int ind) {
-                Log.d("event type index", ind + "");
-
-                if(eventDate.getCheckedRadioButtonId()!=-1){
-                    int id= eventDate.getCheckedRadioButtonId();
-                    View radioButton = eventDate.findViewById(id);
-                    int radioId = radioGroup.indexOfChild(radioButton);
-                    RadioButton btn = (RadioButton) eventDate.getChildAt(radioId);
-                    selectedDate = (String) btn.getText();
-                }
-            }
-        });
+        });*/
         //---------- TIME ------------
-        final EditText startTime = (EditText) view.findViewById(R.id.startTime);
-        startTime.setOnClickListener(new View.OnClickListener() {
 
-            @Override
-            public void onClick(View v) {
-                // TODO Auto-generated method stub
-                Calendar mcurrentTime = Calendar.getInstance();
-                int hour = mcurrentTime.get(Calendar.HOUR_OF_DAY);
-                int minute = mcurrentTime.get(Calendar.MINUTE);
-
-                TimePickerDialog mTimePicker;
-                mTimePicker = new TimePickerDialog(view.getContext(), new TimePickerDialog.OnTimeSetListener() {
-                    @Override
-                    public void onTimeSet(TimePicker timePicker, int selectedHour, int selectedMinute) {
-                        startTime.setText( selectedHour + ":" + selectedMinute);
-                        selectedStartTime = startTime.getText().toString();
-                    }
-                }, hour, minute, true);
-                mTimePicker.setTitle("Starting Time");
-                mTimePicker.show();
-
-            }
-        });
-
-        final EditText endTime = (EditText) view.findViewById(R.id.endTime);
-        endTime.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View v) {
-                // TODO Auto-generated method stub
-                Calendar mcurrentTime = Calendar.getInstance();
-                int hour = mcurrentTime.get(Calendar.HOUR_OF_DAY);
-                int minute = mcurrentTime.get(Calendar.MINUTE);
-
-                TimePickerDialog mTimePicker;
-                mTimePicker = new TimePickerDialog(view.getContext(), new TimePickerDialog.OnTimeSetListener() {
-                    @Override
-                    public void onTimeSet(TimePicker timePicker, int selectedHour, int selectedMinute) {
-                        endTime.setText( selectedHour + ":" + selectedMinute);
-                        selectedEndTime = endTime.getText().toString();
-                    }
-                }, hour, minute, true);
-                mTimePicker.setTitle("Ending Time");
-                mTimePicker.show();
-
-            }
-        });
 
         //-------- LOCATION ----------
-        EditText eventLocation = view.findViewById(R.id.eventPlace);
-        eventLocation.addTextChangedListener(new TextWatcher() {
-
-            @Override
-            public void onTextChanged(CharSequence s, int start, int count, int after) {
-
-            }
-
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count,
-                                          int after) {
-            }
-
-            @Override
-            public void afterTextChanged(Editable s) {
-                if (s != null){
-                    selectedPlace = s.toString();
-                } else {
-                    selectedPlace = "Mystery Location";
-                }
-            }
-        });
 
 
         //-------- DESCRIPTION ----------
-        EditText description = (EditText) view.findViewById(R.id.descriptionText);
+        /*EditText description = (EditText) view.findViewById(R.id.descriptionText);
         description.setFilters(new InputFilter[] {new InputFilter.LengthFilter(200)});
+        */
 
-        description.addTextChangedListener(new TextWatcher() {
+        //-------- BUTTONS ----------
 
-            @Override
-            public void onTextChanged(CharSequence s, int start, int count, int after) {
-
-            }
-
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count,
-                                          int after) {
-            }
-
-            @Override
-            public void afterTextChanged(Editable s) {
-                if (s != null){
-                    selectedDescription = s.toString();
-                } else {
-                    selectedDescription = "Mystery Location";
-                }
+        final Button button = getView().findViewById(R.id.cancelHangout);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), HangoutActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+                startActivity(intent);
             }
         });
-
+        final Button button2 = getView().findViewById(R.id.createHangout);
+        button2.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), HangoutActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+                startActivity(intent);
+            }
+        });
 
         return view;
 
     }
 
+    public void pushDatatoDb(DatabaseReference ref, Hangout h){
+
+        Hangout.EventType eventType = Hangout.EventType.Beer;
+        RadioGroup eventTypeRadioGroup = (RadioGroup) getView().findViewById(R.id.eventTypeRadio);
+        switch(eventTypeRadioGroup.getCheckedRadioButtonId()) {
+            case R.id.beerRadio:
+                eventType = Hangout.EventType.Beer;
+                break;
+            case R.id.coffeeRadio:
+                eventType = Hangout.EventType.Coffee;
+                break;
+            case R.id.foodRadio:
+                eventType = Hangout.EventType.Food;
+                break;
+        }
+
+        RadioGroup dateRadioGroup = (RadioGroup) getView().findViewById(R.id.dateRadio);
+        Calendar calFromTime = Calendar.getInstance(); // creates calendar
+        Calendar calToTime = Calendar.getInstance();
+        calFromTime.setTime(new Date());
+        calToTime.setTime(new Date());
+        switch(dateRadioGroup.getCheckedRadioButtonId()) {
+            case R.id.todayRadio:
+
+                break;
+            case R.id.tomorrowRadio:
+                calFromTime.add(Calendar.DAY_OF_MONTH,1);
+                calToTime.add(Calendar.DAY_OF_MONTH,1);
+                break;
+
+        }
+
+        EditText fromTimeText = getView().findViewById(R.id.fromTimeText);
+        String strFromTime = (String) fromTimeText.getText().toString();
+
+        EditText toTimeText = getView().findViewById(R.id.toTimeText);
+        String strToTime = (String) toTimeText.getText().toString();
+
+        calFromTime.set(Calendar.HOUR,Integer.parseInt(strFromTime.substring(0,1));
+        calFromTime.set(Calendar.MINUTE,Integer.parseInt(strFromTime.substring(3,5)));
+        Date fromTime = calFromTime.getTime();
+        calFromTime.set(Calendar.HOUR,Integer.parseInt(strToTime.substring(0,1))));
+        calFromTime.set(Calendar.MINUTE,Integer.parseInt(strToTime.substring(3,5)));
+        Date toTime = calToTime.getTime();
+        DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+        String strFromDate = dateFormat.format(fromTime).toString();
+        String strToDate = dateFormat.format(toTime).toString();
+        DatabaseReference newRef = ref.push();
+        ArrayList<String> pendingUsers = new ArrayList<String>();
+        //pendingUsers.add("");
+        ArrayList<String> confirmedUsers = new ArrayList<String>();
+        //confirmedUsers.add("");
+        ArrayList<String> rejectedUsers = new ArrayList<String>();
+        //rejectedUsers.add("");
+        ArrayList<String> commentIds = new ArrayList<String>();
+        //commentIds.add("");
+        ArrayList<String> privateMessageIds = new ArrayList<String>();
+        //privateMessageIds.add("");
+
+        String strDescriptionText = "";
+        String strTitle = eventType + " @ " +calFromTime.get(Calendar.HOUR) + " - "+calToTime.get(Calendar.HOUR);
+        EditText descriptionText = getView().findViewById(R.id.descriptionText);
+        String strDescriptionText = (String) descriptionText.getText().toString();
+
+        LatLng location = new LatLng(0,0);
+
+        Hangout hg1 = new Hangout(newRef.getKey(),
+                strTitle,
+                fromTime,
+                toTime,
+                strDescriptionText,
+                location,
+                eventType,
+                FirebaseAuth.getInstance().getCurrentUser().getUid(),
+                pendingUsers,
+                confirmedUsers,
+                rejectedUsers,
+                commentIds,
+                privateMessageIds);
+        newRef.setValue(hg1);
+    }
 
     // TODO: Rename method, update argument and hook method into UI event
     public void onButtonPressed(Uri uri) {
@@ -261,20 +291,6 @@ public class CreateHangoutFragment extends Fragment {
     }
 
 
-    /*public void setTimeOnClick (View view) {
-        Calendar cal = Calendar.getInstance();
-
-        TimePickerDialog timePicker = new TimePickerDialog(this, timePickerListener,
-                cal.get(Calendar.YEAR),
-                cal.get(Calendar.MONTH),
-                cal.get(Calendar.DAY_OF_MONTH));
-        //Create a cancel button and set the title of the dialog.
-        datePicker.setCancelable(false);
-        datePicker.setTitle("Select the date");
-        datePicker.show();
-    }*/
-
-
 
     /**
      * This interface must be implemented by activities that contain this
@@ -290,5 +306,4 @@ public class CreateHangoutFragment extends Fragment {
         // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
     }
-
 }
